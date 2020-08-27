db1c434cdf78ba8dda6970ae345aa9c63a0c649b
KYJ <ouhouh2222@gmail.com>
2020-08-27 22:11:16 +0900

    a

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@ import express from "express";
 import path from "path";
 import ROUTER from "./router/router";
 
-const PORT = 3001;
+const PORT = 3000;
 const app = express();
 
 app.set("view engine", "pug"); 
-// app.use(express.static(path.join(__dirname, "/assets"))); //
+app.use(express.static(path.join(__dirname, "/assets"))); 
 
 app.get("/", ROUTER);
 app.get("/intro", ROUTER);
